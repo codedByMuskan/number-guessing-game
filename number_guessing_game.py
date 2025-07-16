e54109e9eb3480ad504c7a407923c8a7fe2a872b
Muskan kumari <muskan252605@gmail.com>
2025-07-16 21:54:29 +0530

    Add files via upload

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,10 +1,30 @@
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+import random
 
-def sum(a, b):
-    return (a + b)
+secret_number = random.randint(1, 100)
 
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
 
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+guess_count = 0
+
+while True:
+    guess = int(input("Take a guess: "))
+    guess_count += 1
+
+    if guess < secret_number:
+        print("Too low!")
+    elif guess > secret_number:
+        print("Too high!")
+    else:
+        print(f"Correct! You guessed it in {guess_count} tries.")
+        break
+while True:
+    # (put the entire guessing logic here)
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != 'yes':
+        print("Thanks for playing! Goodbye.")
+        break
+
+ 
